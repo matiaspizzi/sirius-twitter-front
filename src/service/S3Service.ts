a4880da9250cc4b3ad98a4015379a2f6de8c9695
ron2452 <matias.nahuel.pizzi@hotmail.com>
2023-12-15 01:16:14 -0300

    Remove S3Service module

--- src/service/S3Service.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const S3Service = {
-  upload: async (file: File, url: string) => {
-    const blob = new Blob([file], { type: "image/png" });
-
-    await axios.put(url, blob);
-  },
-};
